d50a2fcd8d491e2c24b09a6adc6f3e9c4ca8c354
Andrew Faber <afaber003@gmail.com>
2021-05-31 19:26:54 -0700

    just a start

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <list>
+#define test cout << "Here" << endl;
 
 using namespace std;
 
@@ -10,11 +12,17 @@ struct token{
     string word = "";
 };
 
+bool operator< (token lefthandside, token righthandside){
+    if (lefthandside.frequency < righthandside.frequency){
+        return true;
+    } else return false;
+}
+
 // Encrypt with file input to file output
-void encrypt(string fileInputName, string fileOutputName);
+void encrypt(string fileInputName, string fileOutputName, string encoderdatafile);
 
 // Decrypt with file input and file output
-void decrypt(string fileInputName, string fileOutputName);
+void decrypt(string fileInputName, string fileOutputName){}
 
 
 ////////////////////////////////////
@@ -40,7 +48,10 @@ int main(){
             cout << "Enter the name of the output file: ";
             string tempoutput;
             cin >> tempoutput;
-            encrypt(tempinput, tempoutput);
+            cout << "Enter the name of the encrypt data output file: ";
+            string dataoutput;
+            cin >> dataoutput;
+            encrypt("testinput.txt", "testoutput.txt", "testencoder.txt");
         }
 
         // Decrypt from file to file
@@ -66,33 +77,48 @@ int main(){
 
 
 
-void encrypt(string inputFile, string outputFile){
+void encrypt(string inputFile, string outputFile, string encoderdatafile){
     string ans = "";
     string stufftoEncrypt = "";
+    string encoder[20] = {""};
 
 
-    //TODO: READ THE STRING FROM THE intputFile INTO "stufftoEncrypt"
-
-
-
+    //Read input
+    ifstream input;
+    input.open(inputFile);
+    if (!input.is_open()){
+        cout << "File failed to open";
+        return;
+    }
+    string tem = "";
+    while (!input.eof()){
+        getline(input, tem);
+        stufftoEncrypt += tem + "\n";
+    }
+    input.close();
 
     //empty case
     if (stufftoEncrypt == ""){
         return;
     }
-
     //list of words that appear in the string
     vector<token*> tokens;
-    string thingstofind = " ,.-!?;:\"\'()@";
+    string thingstofind = " ,.-!?;:\"\'()@\n";
     unsigned location = 0;
+    for (char i : thingstofind){ //check for start with punctuation
+        if (stufftoEncrypt[location] == i){
+            location++;
+        }
+    }
+    unsigned location2 = 0;
 
     //partitioning of big string into tokens
     topofEncrypt:
-    while (stufftoEncrypt.find_first_of(thingstofind, location + 1) != string::npos){
+    while (stufftoEncrypt.find_first_of(thingstofind, location) < stufftoEncrypt.size()){
         token* newtoken = new token();
-        location = stufftoEncrypt.find_first_of(thingstofind, location + 1);
-        newtoken->word = stufftoEncrypt.substr(location, stufftoEncrypt.find_first_of(thingstofind, location + 1));
-        location = stufftoEncrypt.find_first_of(thingstofind, location + 1);
+        location2 = stufftoEncrypt.find_first_of(thingstofind, location);
+        newtoken->word = stufftoEncrypt.substr(location, stufftoEncrypt.find_first_of(thingstofind, location) - location);
+        location = location2 + 1;
         //checking for if its already in the list of tokens
         for (unsigned i = 0; i < tokens.size(); i++){
             if (newtoken->word == tokens[i]->word){
@@ -103,5 +129,79 @@ void encrypt(string inputFile, string outputFile){
         }
         tokens.push_back(newtoken);
     }
-    
+
+    token* n = new token();
+    for (unsigned i = 0; i < tokens.size(); i++){   //end case
+        n->word = stufftoEncrypt.substr(location, stufftoEncrypt.size()-location);
+        if (n->word == tokens[i]->word){
+            tokens[i]->frequency += 1;
+            delete n;
+            goto endofpartitioning;
+        }
+    }
+    tokens.push_back(n);
+    endofpartitioning:
+
+
+    //create encoder
+    sort(tokens.begin(), tokens.end()); //only uses "<" per c++ reference
+    for (int i = 0; i < 20; i++){ //TODO: replace with hash
+        if (i == tokens.size()){
+            break;
+        }
+        encoder[i] = tokens[i]->word;
+    }
+
+    //start encoding
+    location = 0;
+    for (char i : thingstofind){ //check for start with punctuation
+        if (stufftoEncrypt[location] == i){
+            location++;
+        }
+    }   
+    startagain:
+    while (stufftoEncrypt.find_first_of(thingstofind, location) < stufftoEncrypt.size()){
+        string tempp = "";
+        location2 = stufftoEncrypt.find_first_of(thingstofind, location);
+        tempp = stufftoEncrypt.substr(location, stufftoEncrypt.find_first_of(thingstofind, location) - location);
+        for (int i = 0; i < encoder->size(); i++){
+            if (tempp == encoder[i]){
+                stufftoEncrypt.replace(location, tempp.size(), encoder[i]);
+                location += encoder[i].size() + 1;
+                goto startagain;
+            }
+        }
+        location = location2 + 1;
+    }
+    for (int i = 0; i < encoder->size(); i++){
+        if (stufftoEncrypt.substr(location, stufftoEncrypt.size() - location) == encoder[i]){
+            stufftoEncrypt.replace(location, stufftoEncrypt.size(), encoder[i]);
+        }
+    }
+
+    // print out file
+    ofstream output(outputFile);
+    if (!output.is_open()){
+        cout << "Output file failed to open";
+        return;
+    }
+    output << stufftoEncrypt;
+
+    ofstream encoderoutput(encoderdatafile);
+    if (!encoderoutput.is_open()){
+        cout << "Encoder data file failed to open";
+        return;
+    }
+    encoderoutput << "token, code" << endl;
+    for (int i = 0; i < 20; i++){
+        if (encoder[i] == ""){
+            break;
+        }
+        encoderoutput << encoder[i] << ", " << i+1 << endl;
+    }
+
+    encoderoutput.close();
+    output.close();
+
+
 }
